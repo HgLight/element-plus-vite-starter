1a4b16ad3676d8c121efc2cc796109264b612d6f
HgLight <xyd1041903129@163.com>
2024-02-22 17:26:56 +0800

    chore: 添加axios依赖读取本地配置文件

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
 import { setupStore } from "./store";
+import { getPlatformConfig } from "./config";
 
 // import "~/styles/element/index.scss";
 
@@ -18,7 +19,15 @@ import "uno.css";
 import "element-plus/theme-chalk/src/message.scss";
 
 const app = createApp(App);
-// app.use(ElementPlus);
-setupStore(app);
-app.use(router);
-app.mount("#app");
+
+getPlatformConfig(app).then(async config => {
+    // app.use(ElementPlus);
+    setupStore(app);
+    app.use(router);
+    await router.isReady();
+    app.config.errorHandler = err => {
+      /* 处理错误 */
+      console.log("应用级别错误信息", err);
+    };
+    app.mount("#app");
+  });
