acc00e27b27b0a0f5ef03871d3caba65c64d86cc
HgLight <xyd1041903129@163.com>
2024-02-22 17:22:09 +0800

    fix: 修改ts配置及定义常用类型

---
+++ types/index.d.ts
@@ -0,0 +1,109 @@
+// 此文件跟同级目录的 global.d.ts 文件一样也是全局类型声明，只不过这里存放一些零散的全局类型，无需引入直接在 .vue 、.ts 、.tsx 文件使用即可获得类型提示
+
+type RefType<T> = T | null;
+
+type EmitType = (event: string, ...args: any[]) => void;
+
+type TargetContext = "_self" | "_blank";
+
+type ComponentRef<T extends HTMLElement = HTMLDivElement> =
+  ComponentElRef<T> | null;
+
+type ElRef<T extends HTMLElement = HTMLDivElement> = Nullable<T>;
+
+type ForDataType<T> = {
+  [P in T]?: ForDataType<T[P]>;
+};
+
+type AnyFunction<T> = (...args: any[]) => T;
+
+type PropType<T> = VuePropType<T>;
+
+type Writable<T> = {
+  -readonly [P in keyof T]: T[P];
+};
+
+type Nullable<T> = T | null;
+
+type NonNullable<T> = T extends null | undefined ? never : T;
+
+type Recordable<T = any> = Record<string, T>;
+
+type ReadonlyRecordable<T = any> = {
+  readonly [key: string]: T;
+};
+
+type Indexable<T = any> = {
+  [key: string]: T;
+};
+
+type DeepPartial<T> = {
+  [P in keyof T]?: DeepPartial<T[P]>;
+};
+
+type Without<T, U> = { [P in Exclude<keyof T, keyof U>]?: never };
+
+type Exclusive<T, U> = (Without<T, U> & U) | (Without<U, T> & T);
+
+type TimeoutHandle = ReturnType<typeof setTimeout>;
+
+type IntervalHandle = ReturnType<typeof setInterval>;
+
+type Effect = "light" | "dark";
+
+interface ChangeEvent extends Event {
+  target: HTMLInputElement;
+}
+
+interface WheelEvent {
+  path?: EventTarget[];
+}
+
+interface ImportMetaEnv extends ViteEnv {
+  __: unknown;
+}
+
+interface Fn<T = any, R = T> {
+  (...arg: T[]): R;
+}
+
+interface PromiseFn<T = any, R = T> {
+  (...arg: T[]): Promise<R>;
+}
+
+interface ComponentElRef<T extends HTMLElement = HTMLDivElement> {
+  $el: T;
+}
+
+interface TableButton {
+  buttonName: string; //按钮显示名称：编辑
+  buttonCode: string; //按钮编码：PurchaseInfo.edit
+  buttonIcon: string; //按钮图标：edit
+  buttonType: EpPropMergeType<
+    StringConstructor,
+    | ""
+    | "default"
+    | "warning"
+    | "success"
+    | "info"
+    | "primary"
+    | "danger"
+    | "text",
+    unknown
+  >; //按钮样式：primary
+  buttonClick: string; //按钮点击事件:handleEdit
+  isTableColum: boolean; //是否是表格操作列按钮:false
+  isLink: boolean; //是否为链接按钮:false
+  popconfirmTitle: string; //按钮气泡标题:是否确认删除
+}
+
+interface AxiosResponseDataType<T = any> {
+  code: number;
+  Success: boolean;
+  success: boolean;
+  Message: string;
+  data: T;
+  message: string;
+}
+// function parseInt(s: string | number, radix?: number): number;
+// function parseFloat(string: string | number): number;
